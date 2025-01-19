b15700a56f5f85a2117eff5034f988929cadf3ed
DrZsombi123 <118626431+DrZsombi123@users.noreply.github.com>
2025-01-19 10:26:01 +0100

    commit2

--- fuggveny_HGY_giranzsombor.py
+++ fuggveny_HGY_giranzsombor.py
@@ -1,7 +1,7 @@
 import random
 
-def random_gumik():
+def random_gumik(hossz):
     gyumik = ["alma" , "körte", "szilva", "barack", "málna", "füge", "eper"]
-    return [random.choice(gyumik) for _ in range(30)]
+    return [random.choice(gyumik) for _ in range(hossz)]
 
-print(random_gumik())
+print(random_gumik(5))
